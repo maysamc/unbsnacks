60a305f88038e21df1c859a268bdc3c3e7fd8b04
Carlos André Velozo Trufini <ipox@live.com>
2017-07-03 16:46:53 -0300

    homepage name test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,29 @@
                             <li><a class="color_animation" href="#reservation">Opine</a></li>
                             <li><a class="color_animation" href="#contact">Contato</a></li>
                             <li><a class="color_animation" href="#story">Sobre</a></li>
-                            <li><a class="color_animation" href="{{ url('admin/login') }}">Cadastro/Acessar</a></li>
+							@if (Auth::guest())
+								<li><a class="color_animation" href="{{ url('admin/login') }}">Cadastro/Acessar</a></li>
+							@else
+								<li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+							@endif
                             <!--
                             <?php
                             session_start();
