f4d4a440c8fa091faad1602f45c10a7914d77786
Carlos André Velozo Trufini <ipox@live.com>
2017-07-03 14:22:36 -0300

    fixing contact

--- public/contact.php
+++ public/contact.php
@@ -1,3 +1,22 @@
 <?php
- 
+  //if "email" variable is filled out, send email
+  if (isset($_REQUEST['email']))  {
+  
+  //Email information
+  $admin_email = "ipox@live.com";
+  $email = $_REQUEST['email'];
+  $subject = $_REQUEST['subject'];
+  $comment = $_REQUEST['message'];
+  
+  //send email
+  mail($admin_email, "$subject", $comment, "De:" . $email);
+  
+  //Email response
+  echo "Obrigado!";
+  }
+  
+  //if "email" variable is not filled out, display the form
+  else  {
+	  echo "Favor informar e-mail.";
+  }
 ?>
